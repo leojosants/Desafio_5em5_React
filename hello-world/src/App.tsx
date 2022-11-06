772fb55e2eebb576cbdbbea079ac4c49307578b5
Leonardo Santos <leonardojosantos@gmail.com>
2022-11-06 19:50:56 -0300

    Update App.tsx

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -1,20 +1,28 @@
 // 
+import { useState } from 'react'
 import { Header } from './components/Header/Header' 
 import { Footer } from './components/Footer/Footer'
 
 const App = () => {
-  let name = `Leonardo Santos`;
-  let age = 38;
+  const [name, setName] = useState(`Leonardo Santos`);  
+  const [age, setAge] = useState(10);
+  const [bg, setBg] = useState('#FF0000');
 
-  const handleClick = () => {
-    alert('Clicou na função!');
+  const handle20 = () => {
+    setAge(20);
+  };
+  const handle90 = () => {
+    setAge(90);
   };
 
   return(
-    <div>
+    <div style={{backgroundColor: bg}}>
       <Header name={name} age={age} />
 
-      <button onClick={handleClick}>Clique aqui</button>
+      <button onClick={handle20}>Tenho 20 anos</button>
+      <button onClick={handle90}>Tenho 90 anos</button>
+
+      {age > 30 && 'Eu sou maior de 30 anos'}
 
       <Footer />
     </div>
