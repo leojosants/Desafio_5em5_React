a6ae0bfb504fe890ae32f4bd04b49ed466e9b75f
Leonardo Santos <leonardojosantos@gmail.com>
2022-11-06 20:12:23 -0300

    Finalizado

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -5,24 +5,47 @@ import { Footer } from './components/Footer/Footer'
 
 const App = () => {
   const [name, setName] = useState(`Leonardo Santos`);  
-  const [age, setAge] = useState(10);
+  const [age, setAge] = useState(90);
   const [bg, setBg] = useState('#FF0000');
+
+  const [list, setList] = useState([
+    'JavaScript',
+    'react',
+    'HTML',
+  ]);  
 
   const handle20 = () => {
     setAge(20);
+    setBg('#00FF00');
   };
   const handle90 = () => {
     setAge(90);
+    setBg('#FF0000');
   };
 
   return(
     <div style={{backgroundColor: bg}}>
       <Header name={name} age={age} />
 
-      <button onClick={handle20}>Tenho 20 anos</button>
-      <button onClick={handle90}>Tenho 90 anos</button>
+      {
+        age === 90 && 
+        <button onClick={handle20}>Tenho 20 anos</button>
+      }
 
-      {age > 30 && 'Eu sou maior de 30 anos'}
+      {
+        age === 20 &&
+        <button onClick={handle90}>Tenho 90 anos</button>
+      }
+
+      <hr />
+
+      <ul>
+        {
+          list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))
+        }
+      </ul>
 
       <Footer />
     </div>
