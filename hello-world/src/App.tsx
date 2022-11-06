25ef4ab26de0706f4ddb6254b3f955e73f41414b
Leonardo Santos <leonardojosantos@gmail.com>
2022-11-06 18:00:42 -0300

    Update App.tsx

---
+++ hello-world/src/App.tsx
@@ -0,0 +1,13 @@
+// 
+const App = () => {
+  let name = `Leonardo Santos`;
+  let age = 38;
+
+  return(
+    <div>
+      Me chamo {name} e tenho {age} anos.
+    </div>
+  );
+};
+
+export default App;
