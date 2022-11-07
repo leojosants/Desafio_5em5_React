2068294fd7c44ff4c017156936f1daea0b0995fd
Leonardo Santos <leonardojosantos@gmail.com>
2022-11-06 23:46:05 -0300

    Projeto finalizado

---
+++ todolist/src/components/ListItem/index.tsx
@@ -0,0 +1,23 @@
+// 
+import * as C from './styles';
+import { Item } from '../../types/Item';
+
+type Props = {
+    item: Item; 
+    onChange: (id: number, done: boolean) => void
+}
+
+export const ListItem = ({ item, onChange }: Props) => {
+
+    return(
+        <C.Container done={item.done}>
+            <input 
+                title='#' 
+                type="checkbox" 
+                checked={item.done} 
+                onChange={e => onChange(item.id, e.target.checked)}
+            />
+            <label>{item.name}</label>
+        </C.Container>
+    );
+}
